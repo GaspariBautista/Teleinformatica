9d9b1a0ad839e0c14cf5227b45c52a4365070858
GaspariBautista <ytbauti0@gmail.com>
2025-07-08 15:39:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+    function scrollI() 
+    {
+        const CarruselT = document.getElementById('CarruselT');
+        CarruselT.scrollBy({ left: -300, behavior: 'smooth' });
+    }
+
+    function scrollD() 
+    {
+        const CarruselT = document.getElementById('CarruselT');
+        CarruselT.scrollBy({ left: 300, behavior: 'smooth' });
+    }
